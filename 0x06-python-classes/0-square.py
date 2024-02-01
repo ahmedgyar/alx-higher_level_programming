da2505f0be5ffd1e2491fa54e79cee4534282d3c
ahmedgyar <ahmedyassergyar@gmail.com>
2024-02-01 19:13:37 +0200

    s

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,8 @@
-	#!/usr/bin/python3
+#!/usr/bin/python3
 
-	"""Define a class Square."""
-	class Square:
-	"""Define a class Square."""
-   	 pass
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    pass
