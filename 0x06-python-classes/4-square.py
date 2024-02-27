99e5541ca42669b2bb939de61484f876942df291
ahmedgyar <ahmedyassergyar@gmail.com>
2024-02-27 22:39:40 +0200

    2024-02-27 10:39:40 PM

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,9 +5,9 @@ class Square:
     """Class-Square"""
     def __init__(self, size=0):
         """Constructor of a Square with the size"""
-        if type(size) is not int:
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
-        if (value < 0):
+        if (size < 0):
             raise ValueError("size must be >= 0")
         self.__size = size
 
