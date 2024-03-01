f68ea9f7bca03d66be6ab5f51b667e81ce249b03
ahmedgyar <ahmedyassergyar@gmail.com>
2024-03-01 16:53:51 +0200

    2024-03-01 04:53:51 PM

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,7 +2,5 @@
 """Define a class Rectangle."""
 
 class Rectangle:
-    """Represent a rectangle."""
-    def __init__(self) -> None:
-        pass
-    # Add methods here
+    """empty rectangle class."""
+    pass
