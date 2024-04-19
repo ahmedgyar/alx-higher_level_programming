7e09e3f89471546c84353e6c7c6ddc5cecffd0a8
ahmedgyar <ahmedyassergyar@gmail.com>
2024-04-19 18:26:11 +0200

    2024-04-19 06:26:11 PM

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
-'''script for task 3'''
+''' script for task 2
+    script should take 4 arguments: mysql username,
+    mysql password, database name and state name searched
+'''
 
-import MySQLdb
 import sys
+import MySQLdb
 
 
-def secure_fetch():
-    ''' a safer way to displays all values in the states table of hbtn_0e_0_usa
-        where name matches the argument passed to the script
+def anything():
+    ''' displays all values in the states table in hbtn db where name
+        matches the argument passed to the script
     '''
     username = sys.argv[1]
-    password = sys.argv[2]
+    password = 'ahmedyasser'
     db_name = sys.argv[3]
-    state_name = sys.argv[4]
+    search_name = sys.argv[4]
     host = 'localhost'
     port = 3306
-
-    db = MySQLdb.connect(host=host, user=username, passwd=password,
-                         db=db_name, port=port)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC;",
-                [state_name])
-
-    result = cur.fetchall()
+    
+    con = MySQLdb.connect(user=username, passwd=password,
+                        db=db_name, host=host, port=port)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC;", (search_name,))
+    results = cur.fetchall()
     cur.close()
-    db.close()
+    con.close()
 
-    if result:
-        for row in result:
-            print(row)
+    for result in results:
+        print(result)
 
 
 if __name__ == '__main__':
-    secure_fetch()
+    anything()
