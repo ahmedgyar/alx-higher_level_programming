879fb14e446ccbc7d881475397472d5cd18f0de3
ahmedgyar <ahmedyassergyar@gmail.com>
2024-04-19 17:31:02 +0200

    2024-04-19 05:31:02 PM

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+
+def anything():
+    '''List all states in the database starting with N.'''
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    search_name = sys.argv[4]
+    host = 'localhost'
+    port = 3306
+    con = MySQLdb.connect(user=username, passwd=password,
+                        db=db_name, host=host, port=port)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC;", (search_name,))
+    results = cur.fetchall()
+    cur.close()
+    con.close()
+
+    for result in results:
+        print(result)
+
+
+if __name__ == '__main__':
+    anything()
