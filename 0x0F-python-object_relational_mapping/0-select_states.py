bc554391ed90941cddace7ec821952f780d528dc
ahmedgyar <ahmedyassergyar@gmail.com>
2024-04-19 15:44:23 +0200

    2024-04-19 03:44:23 PM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,40 +3,30 @@ import sys
 import MySQLdb
 
 def anything():
-    # Check if all required arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <db_name>")
-        sys.exit(1)
-
-    # Extracting command line arguments
+    '''list all states in db'''
     username = sys.argv[1]
     password = 'ahmedyasser'
     db_name = sys.argv[3]
     host = 'localhost'
     port = 3306
 
-    # Connecting to the MySQL database
-    try:
-        con = MySQLdb.connect(user=username, passwd=password, db=db_name, host=host, port=port)
-        cur = con.cursor()
+    con = MySQLdb.connect(user=username, passwd=password, db=db_name, host=host, port=port)
+    cur = con.cursor()
 
-        # Executing the SQL query
-        cur.execute("SELECT * FROM states ORDER BY id ASC;")
+    # Executing the SQL query
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
 
-        # Fetching results
-        results = cur.fetchall()
+    # Fetching results
+    results = cur.fetchall()
 
-        # Closing cursor and connection
-        cur.close()
-        con.close()
+    # Closing cursor and connection
+    cur.close()
+    con.close()
 
-        # Printing results
-        for result in results:
-            print(result)
+    # Printing results
+    for result in results:
+        print(result)
 
-    except MySQLdb.Error as e:
-        print("Error connecting to the database:", e)
-        sys.exit(1)
 
 if __name__ == '__main__':
     anything()
